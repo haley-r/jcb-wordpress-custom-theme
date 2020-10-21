6befffa8d54266b9a0527c058c3aa9edb0580388
haley-r <haleyryan77@gmail.com>
2020-10-21 15:57:11 -0500

    menu progress, nothing done just ok save point

--- header.php
+++ header.php
@@ -1,23 +1,30 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width" />
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
     <div id="wrapper" class="hfeed">
-        <header id="header" >
+        <header id="header">
             <div id="branding">
                 <div id="site-title">
-                <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?>
-                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
-                <?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?>
+                    <?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
+                        echo '<h1>';
+                    } ?>
+                    <a href="<?php echo esc_url(home_url('/')); ?>" title="<?php echo esc_html(get_bloginfo('name')); ?>" rel="home"><?php echo esc_html(get_bloginfo('name')); ?></a>
+                    <?php if (is_front_page() || is_home() || is_front_page() && is_home()) {
+                        echo '</h1>';
+                    } ?>
                 </div>
-                <div id="site-description"><?php bloginfo( 'description' ); ?></div>
+                <div id="site-description"><?php bloginfo('description'); ?></div>
             </div>
-            <nav id="menu" >
-                <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+            <nav id="menu" class="main-nav">
+                <button class="menu-toggle"><a href="#top-item">content(s)</a></button>
+                <?php wp_nav_menu(array('theme_location' => 'main-menu')); ?>
             </nav>
         </header>
-    <div id="container">
+        <div id="container">
