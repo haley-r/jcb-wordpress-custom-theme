674640ee1c5b8377262c2c84d5e22b9a246e1276
haley-r <haleyryan77@gmail.com>
2020-10-23 11:15:30 -0500

    i think menu logic is set

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,23 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     let openButton = document.querySelector(".menu-toggle");
-
+    //use the button to designate menu open or not
     openButton.addEventListener("click", () => toggleMenu());
         function toggleMenu() {
         document.querySelector("body").classList.toggle("smallscreen-menu-open");
-        if (openButton.innerHTML!="close"){
+        if(openButton.innerHTML!="close"){
             openButton.innerHTML = "close";
-        }else openButton.innerHTML = "content(s)"
+        }else{
+            openButton.innerHTML = "content(s)";
+            window.scrollTo(0,0);
+        }
     }
-window.addEventListener("resize", function(){
-    if (window.innerWidth>540){
-        openButton.innerHTML = "content(s)";
-        document.querySelector("body").classList.remove("smallscreen-menu-open");
-    }
-    
-})
-
-    //later:
-    //scroll to top of page when closing menu??
-
-    
+    //use resizing to a larger screen to designate menu not open
+    window.addEventListener("resize", function(){
+        if (window.innerWidth>540){
+            openButton.innerHTML = "content(s)";
+            document.querySelector("body").classList.remove("smallscreen-menu-open");
+        }  
+    })
 });
