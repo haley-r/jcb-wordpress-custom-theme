22827e5a1e965b72993a2b0e852be7f219a64086
haley-r <haleyryan77@gmail.com>
2020-11-12 19:34:52 -0600

    single page small mobile styles

--- single.php
+++ single.php
@@ -1,32 +1,33 @@
 <?php get_header(); ?>
-<main id="content">
+<main id="content" class="work-page-content">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <header class="page-header">
                     <h2 class="work-page-title"><?php the_title(); ?></h2>
                     <h3 class="work-page-subtitle"><?php the_field('subtitle') ?></h6>
-                        <?php if (get_post_type() == 'work' && get_field('completion_date') != null) {
-                            $completionDate = new DateTime(get_field('completion_date'));
-                        ?><p class="work-page-completed-date">completed <?php echo $completionDate->format('F Y'); ?></p><?php
-                                                                                                                        } ?>
                 </header>
                 <section class="main-content work-page-main-content">
                     <?php the_post_thumbnail(); ?>
                     <?php the_content(); ?>
-                    <?php if (get_field('link_to_view_work') != null) { ?>
-                        <a href="<?php the_field('link_to_view_work'); ?>" target="_blank" class="custom-view-link">
-                            <?php the_field('label_for_view_link') ?>
-                        </a>
-                    <?php } ?>
+                    <?php if (get_post_type() == 'work' && get_field('completion_date') != null) {
+                        $completionDate = new DateTime(get_field('completion_date'));
+                    ?><p class="work-page-completed-date">completed <?php echo $completionDate->format('F Y'); ?></p><?php
+                       } ?>
+                    <div class="link-block">
+                        <?php if (get_field('link_to_view_work') != null) { ?>
+                            <a href="<?php the_field('link_to_view_work'); ?>" target="_blank" class="custom-view-link">
+                                <?php the_field('label_for_view_link') ?>
+                            </a>
+                        <?php } ?>
 
-                    <?php
-                    $worktypes = get_field('work_type');
-                    if ($worktypes) : 
+                        <?php
+                        $worktypes = get_field('work_type');
+                        if ($worktypes) :
                             foreach ($worktypes as $worktype) : ?>
                                 <a href="<?php echo home_url(); ?>/<?php echo $worktype['value']; ?>">View All <?php echo $worktype['label']; ?></a>
-                            <?php endforeach; 
-                    endif; ?>
-                    <a href="<?php echo home_url(); ?>">Home</a>
+                        <?php endforeach;
+                        endif; ?>
+                        <a href="<?php echo home_url(); ?>">Home</a>
                     </div>
                 </section>
             </article>
