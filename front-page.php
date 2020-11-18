e9c15667a0ba7798520ae51526b878715574a0cf
haley-r <haleyryan77@gmail.com>
2020-11-18 17:01:44 -0600

    splash page works but still short interval

--- front-page.php
+++ front-page.php
@@ -1,8 +1,29 @@
 <?php get_header(); ?>
 <main id="content">
-    <div id="splash-div">
-        <h1 class="splash-title"><?php echo esc_html(get_bloginfo('name')); ?></h1>
-    </div>
+    <script type="text/template" id="splashInsert">
+    </script>
+    <script type="text/javascript">
+        //this is probably extremely unelegant, but it works
+        let now = Date.now();
+        let lastVisited = Number(localStorage.getItem("last-visited"));
+        let timeElapsedInSeconds = (now - lastVisited) / 1000;
+        let splashDiv = document.createElement('DIV');
+        let splashHeader = document.createElement('H1');
+        let splashText = document.createTextNode("Jonah Castañeda Barry");
+        splashHeader.appendChild(splashText);
+        splashHeader.classList.add('splash-title')
+        splashDiv.appendChild(splashHeader);
+        splashDiv.classList.add('splash-div')
+        // if you've recently visited the site, don't even display the splash, just log the time
+        if (localStorage.getItem('last-visited') && timeElapsedInSeconds < 5) {
+            localStorage.setItem("last-visited", Date.now());
+        }
+        //  but if youve never visited, or if its been a while, insert splash into DOM
+        else {
+            localStorage.setItem("last-visited", Date.now());
+            document.querySelector("body").appendChild(splashDiv);
+        }
+    </script>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 <header class="page-header">
