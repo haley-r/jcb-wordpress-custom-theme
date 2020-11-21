e14a8bcfd4991f1fd0ffa5b0f93f4f345bcd84c8
haley-r <haleyryan77@gmail.com>
2020-11-21 16:06:34 -0600

    comment out splash. wont work on phone and I don't know why

--- front-page.php
+++ front-page.php
@@ -1,34 +1,32 @@
 <?php get_header(); ?>
 <main id="content">
-    <script type="text/template" id="splashInsert">
-    </script>
     <script type="text/javascript">
         //this is probably extremely unelegant, but it works
-        let now = Date.now();
-        let lastVisited = Number(localStorage.getItem("last-visited"));
-        let timeElapsedInSeconds = (now - lastVisited) / 1000;
-        let splashDiv = document.createElement('DIV');
-        let splashSubtitle = document.createElement('SPAN');
-        let splashSubtitleText = document.createTextNode('Experimental Multimedia Artist');
-        splashDiv.appendChild(splashSubtitleText);
-        let splashHeader = document.createElement('H1');
-        let splashHeaderText = document.createTextNode('Jonah Castañeda Barry');
-        splashSubtitle.appendChild(splashSubtitleText);
-        splashSubtitle.classList.add('splash-subtitle');
-        splashHeader.appendChild(splashHeaderText);
-        splashHeader.appendChild(splashSubtitle);
-        splashHeader.classList.add('splash-title');
-        splashDiv.appendChild(splashHeader);
-        splashDiv.classList.add('splash-div');
+        // let now = Date.now();
+        // let lastVisited = Number(localStorage.getItem("last-visited"));
+        // let timeElapsedInSeconds = (now - lastVisited) / 1000;
+        // let splashDiv = document.createElement('DIV');
+        // let splashSubtitle = document.createElement('SPAN');
+        // let splashSubtitleText = document.createTextNode('Experimental Multimedia Artist');
+        // splashDiv.appendChild(splashSubtitleText);
+        // let splashHeader = document.createElement('H1');
+        // let splashHeaderText = document.createTextNode('Jonah Castañeda Barry');
+        // splashSubtitle.appendChild(splashSubtitleText);
+        // splashSubtitle.classList.add('splash-subtitle');
+        // splashHeader.appendChild(splashHeaderText);
+        // splashHeader.appendChild(splashSubtitle);
+        // splashHeader.classList.add('splash-title');
+        // splashDiv.appendChild(splashHeader);
+        // splashDiv.classList.add('splash-div');
         // if you've recently visited the site, don't even display the splash, just log the time
-        if (localStorage.getItem('last-visited') && timeElapsedInSeconds < 3600) {
-            localStorage.setItem("last-visited", Date.now());
-        }
+        // if (localStorage.getItem('last-visited') && timeElapsedInSeconds < 3600) {
+        //     localStorage.setItem("last-visited", Date.now());
+        // }
         //  but if youve never visited, or if its been a while, insert splash into DOM
-        else {
-            localStorage.setItem("last-visited", Date.now());
-            document.querySelector("body").appendChild(splashDiv);
-        }
+        // else {
+        //     localStorage.setItem("last-visited", Date.now());
+        //     document.querySelector("body").appendChild(splashDiv);
+        // }
     </script>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
