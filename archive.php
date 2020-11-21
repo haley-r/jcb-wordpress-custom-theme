8639929ae4c945aebeba12838273ef797101f2bf
haley-r <haleyryan77@gmail.com>
2020-11-21 17:52:06 -0600

    archive styles

--- archive.php
+++ archive.php
@@ -1,22 +1,23 @@
 <?php get_header(); ?>
 <main id="content" class="work-index-content">
+    <h1 class="index-title">Index of Work</h1>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <h2 class="work-page-title">
+                <h2>
                     <a href="<?php echo get_permalink() ?>">
                         <?php the_title(); ?>
                     </a>
+                    <?php $worktypes = get_field('work_type');
+                    if ($worktypes) :
+                        foreach ($worktypes as $worktype) :  ?>
+                            <span class="work-type"><a href="<?php echo home_url(); ?>/<?php echo $worktype['value']; ?>">(<?php echo $worktype['label']; ?>)</a></span>
+                    <?php endforeach;
+                    endif; ?>
+                    <?php if (get_post_type() == 'work' && get_field('completion_date') != null) {
+                        $completionDate = new DateTime(get_field('completion_date'));
+                    ?><span class="work-page-completed-date">- <?php echo $completionDate->format('Y'); ?></span><?php
+                    } ?>
                 </h2>
-                <?php $worktypes = get_field('work_type');
-                if ($worktypes) :
-                    foreach ($worktypes as $worktype) :  ?>
-                        <a href="<?php echo home_url(); ?>/<?php echo $worktype['value']; ?>">(<?php echo $worktype['label']; ?>)</a>
-                <?php endforeach;
-                endif; ?>
-                <?php if (get_post_type() == 'work' && get_field('completion_date') != null) {
-                    $completionDate = new DateTime(get_field('completion_date'));
-                ?><p class="work-page-completed-date"><?php echo $completionDate->format('Y'); ?></p><?php
-                                                                                                        } ?>
             </article>
     <?php endwhile;
     endif; ?>
