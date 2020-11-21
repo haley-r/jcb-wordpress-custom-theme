7839a2baef093d9e274150da080597bb10b11f1a
haley-r <haleyryan77@gmail.com>
2020-11-21 17:36:50 -0600

    404 styling

--- 404.php
+++ 404.php
@@ -1,10 +1,11 @@
 <?php get_header(); ?>
 <main id="content">
-<article id="post-0" class="post not-found">
-    <header class="header">
-        <h1 class="entry-title">404</h1>
-        <p>Page not found. <a>go to homepage</a></p>
-    </header>
-</article>
+    <article id="post-0" class="post not-found">
+        <header class="header">
+            <h1 class="entry-title">404</h1>
+            <p>Page not found.</p>
+            <a href="<?php echo home_url(); ?>">go to homepage</a>
+        </header>
+    </article>
 </main>
 <?php get_footer(); ?>
