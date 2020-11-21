e884a8e981c6e69cc74e9d012c04cf3055085c94
haley-r <haleyryan77@gmail.com>
2020-11-21 17:25:07 -0600

    404 changes. not styled yet

--- 404.php
+++ 404.php
@@ -1,14 +1,10 @@
 <?php get_header(); ?>
 <main id="content">
 <article id="post-0" class="post not-found">
-<header class="header">
-<h1 class="entry-title"><?php esc_html_e( 'Not Found', 'blankslate' ); ?></h1>
-</header>
-<div class="entry-content">
-<p><?php esc_html_e( 'Nothing found for the requested page. Try a search instead?', 'blankslate' ); ?></p>
-<?php get_search_form(); ?>
-</div>
+    <header class="header">
+        <h1 class="entry-title">404</h1>
+        <p>Page not found. <a>go to homepage</a></p>
+    </header>
 </article>
 </main>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
