8e71ce85a8af4f04b73db37f94cc0a7665470a11
haley-r <haleyryan77@gmail.com>
2020-11-21 17:18:37 -0600

    archive template, not styled yet

--- archive.php
+++ archive.php
@@ -1,13 +1,24 @@
 <?php get_header(); ?>
-<main id="content">
-<header class="header">
-<h1 class="entry-title"><?php single_term_title(); ?></h1>
-<div class="archive-meta"><?php if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } ?></div>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
+<main id="content" class="work-index-content">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h2 class="work-page-title">
+                    <a href="<?php echo get_permalink() ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h2>
+                <?php $worktypes = get_field('work_type');
+                if ($worktypes) :
+                    foreach ($worktypes as $worktype) :  ?>
+                        <a href="<?php echo home_url(); ?>/<?php echo $worktype['value']; ?>">(<?php echo $worktype['label']; ?>)</a>
+                <?php endforeach;
+                endif; ?>
+                <?php if (get_post_type() == 'work' && get_field('completion_date') != null) {
+                    $completionDate = new DateTime(get_field('completion_date'));
+                ?><p class="work-page-completed-date"><?php echo $completionDate->format('Y'); ?></p><?php
+                                                                                                        } ?>
+            </article>
+    <?php endwhile;
+    endif; ?>
 </main>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
